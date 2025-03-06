f8ab5132c1701724b007dcaa3bd2144f788f7ff4
aditya pathak <pathakaditya881@gmail.com>
2025-03-06 20:57:44 +0530

    updated

--- dist/class.py
+++ dist/class.py
@@ -86,16 +86,119 @@
 #Types of Inheritance 
 #single,multiple,multilevel ,hirarichial inheritance
 
+# class Parent:
+#     x=10
+#     def car(self):
+#         print("Parent Home")
+        
+# class Child(Parent):
+#     y=20
+#     def car(self):
+#         super().car()
+#         print("Child Car")
+        
+# obj=Child()
+# print(obj.x)
+# obj.car()
+
+#if 2 metyhods in p-c class have same name then it is called as functioon overriding..
+
+
+# class A:
+#     def add(self,x,y,z):
+#         print(x+y+z)
+        
+#     def add(self,x):
+#         print(x)
+        
+#     def add(self,x,y):
+#         print(x+y)
+        
+# obj=A()
+# obj.add(2,3)
+
+
+#multiple inheritance...child class usses MRO technique
+
+# class Parent1:
+#     def home(self):
+#         print("parent1 home")
+        
+# class Parent2:
+#     def home(self):
+#         print("parent2 home")
+        
+# class Child(Parent2,Parent1):
+#     def car(self):
+#         print("child car")
+        
+# obj=Child()
+# obj.home()
+
+#Multilevel inheritance
+
+# class Grand_parent:
+#     def home(self):
+#         print("parent1 home")
+        
+# class Parent(Grand_parent):
+#     def car(self):
+#         print("parent2 car")
+        
+# class Child(Parent):
+#     def new(self):
+#         print("child new")
+        
+# obj=Child()
+# obj.new()
+# obj.car()
+# obj.home()
+
+#hierarical inheritance
+
+# class Parent:
+#     def home(self):
+#         print("from parent class")
+    
+# class Child1(Parent):
+#     def home(self):
+#         #print("from child1 class")
+#         super().home()
+        
+# class Child2(Parent):
+#     def home(self):
+#         print("from child2 class")
+        
+# obj=Child1()
+# obj.home()
+# obj=Child2()
+# obj.home()
+
+#Hybrid inheritance#################
+
+
+
 class Parent:
-    x=10
     def home(self):
-        print("Parent Home")
+        print("from parent class")
+    
+class Child1(Parent):
+    def home(self):
+        print("from child1 class")
+        super().home()
         
-class Child(Parent):
-    y=20
-    def car(self):
-        print("Child Car")
+class Child2(Parent):
+    def home(self):
+        print("from child2 class")      
         
-obj=Child()
-print(obj.x)
+class Child3(Child1,Child2):
+    def new(self):
+        print("from child3")
+        
+obj=Child3()
 obj.home()
+print(Child3.__mro__)  
+
+
+
+
