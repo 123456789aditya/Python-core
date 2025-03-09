2ed48bf163b48e93a979c6974df595da5c88a718
aditya pathak <pathakaditya881@gmail.com>
2025-03-09 21:49:52 +0530

    added

--- dist/fh.py
+++ dist/fh.py
@@ -5,7 +5,21 @@
 # print(f.closed)
 # print(f.readable())
 
-f=open("n2.txt",'a')
-data='this is python class'
-f.write(data)
-f.close()
+# f=open("n2.txt",'a')
+# data='this is python class'
+# f.write(data)
+# f.close()
+
+f=open('n1.txt','r')
+data=f.read()
+print(data)
+print(f.readline())
+
+
+
+
+
+
+
+
+
