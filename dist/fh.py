1a8d275778d10aa9df97142f1649c574dbfd0a83
aditya pathak <pathakaditya881@gmail.com>
2025-03-12 23:31:02 +0530

    added

--- dist/fh.py
+++ dist/fh.py
@@ -1,22 +1,22 @@
-# f=open('n2.txt','a')
-# print(f.name)
-# print(f.mode)
-# print(f.writable())
-# print(f.closed)
-# print(f.readable())
+f=open('n2.txt','a')
+print(f.name)
+print(f.mode)
+print(f.writable())
+print(f.closed)
+print(f.readable())
 
-# f=open("n2.txt",'a')
-# data='this is python class'
-# f.write(data)
-# f.close()
+f=open("n2.txt",'a')
+data='this is python class'
+f.write(data)
+f.close()
 
-# f=open("n1.txt",'rb')
-# # data='Aditya pathak will be a dictator of India'
-# data=f.read()
-# print(data)
-# print(f.tell())
-# f.seek(-10,2)
-# print(f.tell())
+f=open("n1.txt",'rb')
+# data='Aditya pathak will be a dictator of India'
+data=f.read()
+print(data)
+print(f.tell())
+f.seek(-10,2)
+print(f.tell())
 
 
 
