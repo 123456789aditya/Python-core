4d2ac6625a7d000b963a2230d707dedee5cb64ae
aditya pathak <pathakaditya881@gmail.com>
2025-03-18 00:04:46 +0530

    added

--- Modular/first.py
+++ Modular/first.py
@@ -1,6 +1,55 @@
-# def add(x,y,z):
-#     return x+y+z
+# # def add(x,y,z):
+# #     return x+y+z
 
-# def display():
-#     print("hi")
+# # def display():
+# #     print("hi")
 
+# from abc import ABC,abstractmethod
+
+# class Computer(ABC):
+#     @abstractmethod
+#     def process(self):
+#         pass
+
+# class Laptop():
+#     def one(self):
+#         print("its running")
+        
+        
+# class Program(Computer):
+#     def process(self,com):
+#         print("Solving Bugs")
+        
+        
+        
+# com=Computer()        
+# p1=Program()
+# p1.process()
+
+from abc import ABC,abstractmethod
+
+class BankApp(ABC):
+    def login(self):
+        print("user login")
+    
+    def logout(self):
+        print("user logout")
+        
+    def userdetail(self):
+        print("user details")
+        
+    @abstractmethod
+    
+    def database(self):
+        pass
+    
+class WebPage(BankApp):
+    def database(self):
+        print("database connected")
+        
+        
+obj=WebPage()
+obj.login()
+obj.logout()
+obj.userdetail()
+obj.database()
