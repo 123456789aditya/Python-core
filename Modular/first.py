48332e9a2bc78f9d90a27f124269cc1d0d9bbf6c
aditya pathak <pathakaditya881@gmail.com>
2025-03-19 11:43:16 +0530

    added

--- Modular/first.py
+++ Modular/first.py
@@ -81,18 +81,151 @@
 # obj.car()
 # obj.home()
 
-class Chasis:
-    def home(self):
-        print("car basic structure")
+# class Chasis:
+#     def home(self):
+#         print("car basic structure")
         
-class Engine(Chasis):
-    def engine(self):
-        print("car engine design")
+# class Engine(Chasis):
+#     def engine(self):
+#         print("car engine design")
         
-class Car(Engine):
-    def screen(self):
-        print("car price 500000")
+# class Car(Engine):
+#     def screen(self):
+#         print("car price 500000")
         
-C1=Car()
-C1.home()
-C1.engine()
+# C1=Car()
+# C1.home()
+# C1.engine()
+
+
+
+
+# class Human:
+#     def __init__(self,name,age):
+#         print("calling init from Human class")
+#         self.name=name
+#         self.age=age
+        
+#     def show(self):
+#         print(f"name:{self.name}, age is:{self.age} ")
+        
+#     def eat(self):
+#         print("I can eat")
+        
+# class Male(Human):
+#     def __init__(self,name,age,location):
+#         Human.__init__(self,name,age)
+#         self.location=location
+#         print(f"{self.name}{self.age}{self.location}")
+        
+#     def sleep(self):
+#         print("i can slep whole day")
+        
+# class Female(Human):
+    
+#     def __init__(self,name,age,dance):
+#         print("calling from female class")
+#         super().__init__(name,age)
+#         self.dance=dance
+#         print(f"{self.name, self.age, self.dance}")
+       
+#     def work(self):
+#         print("i can code")
+        
+# # f1=Female("sharon",21)
+# # f1.show()
+# # f1.eat()
+
+
+# m1=Female("Sharon",21,"dance")
+    
+    
+        
+        
+
+
+
+
+
+# a=lambda x,y : (x+y)
+# p=10
+# q=20
+# print(a(p,q))
+
+
+# n=int(input("enter any num"))
+# check_num =lambda x:  'even no' if (x%2==0)  else 'odd'
+# print(check_num(n))
+
+# b=str(input("enter first password:"))
+# c=str(input("enter passowrd again:"))
+
+# res=lambda n,a: "passowrd matched" if n==a else "incorrect password"
+# print(res(b,c))
+
+
+# l1=[1,2,3,4,5,6,7,8,9,10]
+
+# def display(x):
+#     if x%2==0:
+#         return x
+    
+        
+# res=filter(display,l1)
+# print(list(res))
+    
+# def even(x):
+#     if x%2==0:
+#         return 'even'
+#     else:
+#         return 'odd'
+    
+# l1=[1,20,30,0,40,50,60]
+# res=filter(even,l1)
+# print(list(res))
+
+
+# def display(a):
+#     if a>10:
+#         return a
+    
+# l1=[1,2,3,10,20,30,40]
+# res=filter(display,l1)
+# print(list(res))
+
+
+# l1=[1,2,3,4,5,6,6,7,9,10]
+# x=filter(lambda a: a if a>3 else None,l1)
+# print(list(x))
+
+# l1=[1,2,3,4,5,6,7,8,9,10]
+# print(list(filter(lambda a: a if a>3 else None,l1)))
+
+
+
+# from functools import reduce
+# def display(x,y):
+#     return x+y
+    
+# l1=[1,2,3,4,5,6,7,8,9]
+# res=reduce(display,l1)
+# print(res)
+
+# def div(a,b):
+#     print(a/b)
+    
+# def smart(func):
+#     def inner(a,b):
+#         if a<b:
+#             a,b=b,a
+#         return func(a,b)
+#     return inner
+
+# div=smart(div)
+# print(div(2,4))
+    
+      
+
+    
+
+
