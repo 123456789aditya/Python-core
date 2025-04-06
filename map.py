d6518ba1e9e1eda2495ed9d7cda630048584fd2b
aditya pathak <pathakaditya881@gmail.com>
2025-04-06 22:32:40 +0530

    add

--- map.py
+++ map.py
@@ -1,34 +1,28 @@
 
 
-# def squr(x):
-#     return x*x
+def squr(x):
+    return x*x
 
 
-# n=eval(input("enter any number:"))
-# res=map(squr,n)
-# print(list(res))
+n=eval(input("enter any number:"))
+res=map(squr,n)
+print(list(res))
 
-# def display(n):
-#     if n%2==0:
-#         return n+1
-#     else:
-#         return n+2
+def display(n):
+    if n%2==0:
+        return n+1
+    else:
+        return n+2
 
 
-# a=eval(input("enter numbers from 1 to 20"))
-# result=map(display,a)
-# print(list(result))
+a=eval(input("enter numbers from 1 to 20"))
+result=map(display,a)
+print(list(result))
 
 
 # print("sankat te hanuman chhudawe man kram vachan dhyaan jo laave")
 
 
-# l1=[2,4,6]
-# l2=[1,2,3]
-# def display(x,y):
-#     return x**y
 
-# result=map(display,l1,l2)
-# print(list(result))
 
 
