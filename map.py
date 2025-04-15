b1033bd179fb81be0aa2812ba2d0349a0ca542ff
aditya pathak <pathakaditya881@gmail.com>
2025-04-15 23:51:33 +0530

    update

--- map.py
+++ map.py
@@ -24,24 +24,24 @@ print(list(result))
 
 
 
-def squr(x):
-    return x*x
+# def squr(x):
+#     return x*x
 
 
-n=eval(input("enter any number:"))
-res=map(squr,n)
-print(list(res))
+# n=eval(input("enter any number:"))
+# res=map(squr,n)
+# print(list(res))
 
-def display(n):
-    if n%2==0:
-        return n+1
-    else:
-        return n+2
+# def display(n):
+#     if n%2==0:
+#         return n+1
+#     else:
+#         return n+2
 
 
-a=eval(input("enter numbers from 1 to 20"))
-result=map(display,a)
-print(list(result))
+# a=eval(input("enter numbers from 1 to 20"))
+# result=map(display,a)
+# print(list(result))
 
 
 
