f4b179e698dc111ad5e6fe50820e16a1a581b0d8
aditya pathak <pathakaditya881@gmail.com>
2025-05-22 23:35:32 +0530

    add

--- basics/filter.py
+++ basics/filter.py
@@ -4,20 +4,20 @@
 #     else:
 #         return 'odd'
     
-l1=[1,20,30,0,40,50,60]
-res=filter(even,l1)
-print(list(res))
+# l1=[1,20,30,0,40,50,60]
+# res=filter(even,l1)
+# print(list(res))
 
 
-reduce............
+# reduce.........................
 
-from functools import reduce
-def display(x,y):
-    return x+y
+# from functools import reduce
+# def display(x,y):
+#     return x+y
     
-l1=[1,2,3,4,5,6,7,8,9]
-res=reduce(display,l1)
-print(res)
+# l1=[1,2,3,4,5,6,7,8,9]
+# res=reduce(display,l1)
+# print(res)
 
 
 
